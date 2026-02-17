eed18687592b79c2676decba8399a6bc34ea8c98
CRON997 <dmitriy.rebrikov2006@gmail.com>
2026-02-17 16:29:43 +0200

    feat(main): implement initial models and admin configuration

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,52 @@
+import datetime
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Project(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+        ordering = ['name']
+
+
+class Task(BaseModel):
+    name = models.CharField(max_length=100)
+
+    done = models.BooleanField(default=False)
+    deadline = models.DateTimeField(null=True, blank=True)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Task {self.name} for {self.project}'
+
+    def clean(self):
+        super().clean()
+
+        from django.utils import timezone
+        if self.deadline and self.deadline < timezone.now():
+            raise ValidationError({
+                "deadline": (
+                    'Date cannot be'
+                )
+            })
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        ordering = ['name', 'deadline']
+
